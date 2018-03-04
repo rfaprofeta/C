43159b4484e8875dce50ddedfa768739d5643efd
Rafael Profeta <rebaellion@gmail.com>
2018-03-04 08:53:09 -0300

    Adicionando o srand

--- matriz2.c
+++ matriz2.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
 	int n,i,j,somax=0,somay=0;
 	printf("Digite o range da matriz\n");
 	scanf("%d",&n);
 	int mat[n][n];
+	srand(time(NULL));
 	for(i=0;i<n;i++)
 		for (j=0;j<n;j++)
-			mat[i][j]=rand()%10;
+			mat[i][j]=rand()%100;
 	for (i=0;i<n;i++)
 		{
 		for (j=0;j<n;j++)

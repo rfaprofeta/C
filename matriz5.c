b08057fc2bef22036b85ee037bbf1b6681124a87
Rafael <rafael@gmail.com>
2018-03-14 22:21:31 -0300

    Conclusão

---
+++ matriz5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	int n,hz=0,i,j,parprim=0,parsec=0,somax=0,somay=0;
+	FILE *pont_arq;// criando variável para imprimir
+	system("clear");
+	//printf("Digite o range da matriz\n");
+	//scanf("%d",&n);
+	pont_arq = fopen("matriz.txt","w");
+	n=1447;
+	int mat[n][n];
+	srand(time(NULL));
+	for(i=0;i<n;i++)
+		for (j=0;j<n;j++){
+			mat[i][j]=rand()%2000;
+			hz+=mat[i][j];
+		}
+	for (i=0;i<n;i++)
+		{
+		for (j=0;j<n;j++)
+			fprintf(pont_arq,"%p\t",mat[i][j]);
+		fprintf(pont_arq,"\n");
+		}
+	fclose(pont_arq);
+	for (i=0,j=0;i<n,j<n;i++,j++){
+		somax+=mat[i][j];
+		if(mat[i][j]%2==0)
+			parprim+=mat[i][j];
+		}
+	printf("A soma da diagonal principal é %d\n",somax);
+	printf("A dos números pares da diagonal primária é %d\n",parprim);
+	for (i=0,j=n-1;i<=n-1,j>=0;i++,j--){
+		somay+=mat[i][j];
+		if(mat[i][j]%2==0)
+			parsec+=mat[i][j];
+		}
+	printf("A soma da diagonal secundária é %d\n",somay);
+	printf("A soma dos números pares da diagonal secundária é %d\n",parsec);
+	printf("O valor total da matriz é %d\n",hz);
+	printf("By Profeta\n\n\n");
+return 0;
+}
